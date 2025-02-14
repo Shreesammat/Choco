25f8ac9e4cc4be36921b578d868f61d961bd4899
midsane <adityaraj10544@gmail.com>
2025-02-14 19:02:29 +0000

    added notebox colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,10 +8,17 @@ export default {
     extend: {
       colors: {
         primary: "",
-        secondary: "",
+        secondary: "#e7e5e4",
         label: "",
         accent: "",
-        background: ""
+        background: "white",
+        cgreen: "#58cc02",
+        cblack: "#131f24",
+        cblue: "#47b8ee",
+        cred: "#ff4b4b",
+        cyellow: "#ffd900",
+        cpurple: "#ce82ff",
+        cwhite: "#d2d7dd"
       }
 
     },
