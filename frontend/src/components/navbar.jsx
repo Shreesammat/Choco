7160aea0149c77a889004c8040688e8371e899c6
midsane <adityaraj10544@gmail.com>
2025-02-14 21:19:50 +0000

    added search bar

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -86,7 +86,14 @@ export const ProfileNavbar = ({ setKeepTheLatestNotesHidden }) => {
     }
     return (
         <>
-            <div className="fixed top-0 z-40 left-0 w-screen h-14 items-center bg-white">
+            <div className="fixed top-0 z-40 py-2 left-0 pl-36 w-screen h-14 items-center bg-white">
+                <div className="bg-transparent border-2 items-center rounded-3xl flex gap-2 justify-between w-fit h-fit">
+                    <input placeholder="search here" className="bg-transparent focus:outline-none rounded-3xl px-10 py-1" ></input>
+                    <div className="hover:bg-slate-100 active:scale-90 ease-linear duration-75 p-2 cursor-pointer rounded-full">
+                        <SearchIcon />
+                    </div>
+                </div>
+
             </div>
             <div className="fixed h-screen w-28 z-50 flex flex-col justify-start gap-2 items-center left-0 top-0 bg-white border-r-stone-200 border">
                 <div className="flex gap-2 justify-start h-fit items-center p-3 pt-4 pb-10">
@@ -144,7 +151,7 @@ const Bulletcolor = ({ color, borderColor, delay, setKeepTheLatestNotesHidden })
     )
 
     useEffect(() => {
-        if(!startAnimate) return;
+        if (!startAnimate) return;
         const timeout = setTimeout(() => {
             setStartAnimate(false)
         }, 1000);
@@ -171,7 +178,7 @@ const Bulletcolor = ({ color, borderColor, delay, setKeepTheLatestNotesHidden })
 
     return <div className="relative">
         <motion.div
-            initial={{ y: -40, opacity: 0}}
+            initial={{ y: -40, opacity: 0 }}
             animate={{ y: 0, opacity: 1 }}
             exit={{ y: -40, opacity: 0 }}
             transition={{ delay, type: "spring" }}
@@ -180,7 +187,7 @@ const Bulletcolor = ({ color, borderColor, delay, setKeepTheLatestNotesHidden })
         </motion.div>
         {startAnimate && <motion.div
             initial={{ opacity: 0, top: "13rem", left: "2rem", width: "1rem", height: "1rem" }}
-            animate={{ opacity: 1, top: "5rem", left:"9rem",  width: dimension.width, height: dimension.height, borderRadius:"0.5rem" }}
+            animate={{ opacity: 1, top: "5rem", left: "9rem", width: dimension.width, height: dimension.height, borderRadius: "0.5rem" }}
             exit={{ opacity: 0 }}
             onClick={handleClick}
             className={`rounded-full fixed cursor-pointer active:scale-75 ease-linear duration-75  border border-${borderColor} aspect-square ${color}`}>
--- frontend/src/components/notebox.jsx
+++ frontend/src/components/notebox.jsx
@@ -1,4 +1,5 @@
 import { motion } from "framer-motion";
+import EditIcon from '@mui/icons-material/Edit';
 export const NoteBox = ({ noteObj, delay, keepTheLatestNotesHidden }) => {
     let color;
     switch (noteObj.colorType) {
@@ -22,7 +23,7 @@ export const NoteBox = ({ noteObj, delay, keepTheLatestNotesHidden }) => {
         initial={{ x: -50, opacity: 0 }}
         animate={{ x: 0, opacity: 1 }}
         transition={{ delay, type: "spring" }}
-        className={`sm:max-w-80 max-w-[350px] w-full p-3 h-full min-h-52 ${color} ${keepTheLatestNotesHidden && delay === 0 && 'invisible'} rounded-lg noteBox`}>
+        className={`sm:max-w-80 max-w-[350px] w-full flex flex-col p-5 h-full min-h-52 ${color} ${keepTheLatestNotesHidden && delay === 0 && 'invisible'} rounded-lg noteBox`}>
         <div className="h-[90%]" >
             {keepTheLatestNotesHidden && delay === 0 ?
                 <p className="text-stone-800 text-lg">{noteObj.text}</p>
@@ -30,21 +31,24 @@ export const NoteBox = ({ noteObj, delay, keepTheLatestNotesHidden }) => {
                 <motion.p
                     initial={{ y: -10, opacity: 0 }}
                     animate={{ y: 0, opacity: 1 }}
-                    className="text-stone-700">{noteObj.text}</motion.p>
+                    className="text-stone-900 text-xl">{noteObj.text}</motion.p>
 
             }
         </div>
 
-        <div className="flex justify-between">
+        <div className="flex justify-between items-center">
             {keepTheLatestNotesHidden && delay === 0 ?
-                <p className="text-stone-800 text-lg">{noteObj.createdDate}</p>
+                <p className="text-stone-800 text-lg ">{noteObj.createdDate}</p>
                 :
                 <motion.p
                     initial={{ y: -10, opacity: 0 }}
                     animate={{ y: 0, opacity: 1 }}
-                    className="text-stone-700">{noteObj.createdDate}</motion.p>
+                    className="text-stone-800 text-lg opacity-50">{noteObj.createdDate}k</motion.p>
 
             }
+            <div className="ease-linear duration-75 active:scale-90 p-1 rounded cursor-pointer bg-slate-100 bg-opacity-50">
+                <EditIcon sx={{ color: "black" }} />
+            </div>
         </div>
     </motion.div>)
 }
