851787686e5204141edc5e744d4a3cdc45e12b48
midsane <adityaraj10544@gmail.com>
2025-02-15 14:10:59 +0000

    add more feauters in folder page section

--- frontend/src/components/folder.jsx
+++ frontend/src/components/folder.jsx
@@ -1,26 +1,31 @@
-import { EditIcon, FolderIcon, SaveIcon } from "lucide-react"
+import { EditIcon, FolderEdit, FolderIcon, SaveIcon } from "lucide-react"
 import { motion } from "framer-motion"
-import { useState } from "react"
-export const Folder = ({ folderObj, delay }) => {
-    const [isEditing, setIsEditing] = useState(folderObj.draft)
-    const openFolder = () => {
+import { useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
 
+export const Folder = ({ folderObj, delay }) => {
+    const navigate = useNavigate()
+    const [isEditing, setIsEditing] = useState(folderObj.draft)
+    const[folderName, setFolderName] = useState(folderObj.name ? folderObj.name : "")
+  
+    const openFolder = () => {
+        navigate(folderObj.name)
     }
 
     const  saveFolderName = () => {
-            
-    }
+        
+    }   
 
     return <motion.div
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ delay }}
-        onClick={openFolder}
+        onClick={folderObj.name && openFolder}
         className="flex flex-col border active:scale-95 ease-linear duration-75 px-2 py-4 hover:border-stone-400 rounded-xl max-w-60 min-w-32 gap-2 cursor-pointer">
         <FolderIcon />
         {isEditing ?
             <div className="flex w-full px-1 justify-between">
-                <input className="w-fit bg-slate-100 border-stone-300 p-1 rounded border" placeholder="folder name" />
+                <input value={folderName} onChange={(e) => setFolderName(e.target.value)} className="w-fit bg-slate-100 border-stone-300 p-1 rounded border" placeholder="folder name" />
                 <SaveIcon onClick={saveFolderName} className="active:scale-95 text-green-500 ease-linear duration-100" />
             </div>
             :
--- frontend/src/hooks/useFetch.js
+++ frontend/src/hooks/useFetch.js
@@ -40,7 +40,6 @@ export const useCustomFetch = (url) => {
     const [data, setData] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
-
     useEffect(() => {
         const fetchData = async () => {
             try {
@@ -63,3 +62,36 @@ export const useCustomFetch = (url) => {
     return { data, loading, error };
 };
 
+
+export const useCustomPost = (url) => {
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    useEffect(() => {
+        const fetchData = async (bodyData) => {
+            try {
+                const response = await fetch(url, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                         Authorization: `Bearer ${localStorage.getItem('token')}`
+                    },
+                    body: JSON.stringify(bodyData)
+                });
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                const result = await response.json();
+                setData(result);
+            } catch (error) {
+                setError(error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchData();
+    }, [url]);
+
+    return { data, loading, error };
+};
